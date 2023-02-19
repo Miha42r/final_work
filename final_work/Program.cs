1e0c256a82b51e80d898819188dadd69eba0401b
Miha_GB <m-stogov@yandex.ru>
2023-02-19 18:13:25 +0700

    Обнаружил не верную реализацию, устранил.

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -14,20 +14,40 @@ string[] Input()
     return mass;
 }
 
+void PrintArray(string[] array)
+{
+    Console.WriteLine("New array data");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 
 string[] str = Input();
 FindElementsLessThree(str);
 void FindElementsLessThree(string[] str)
 {
+int count = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            count++;
+        }
+        //Console.WriteLine($"This is elements less three: {str2[j]}");
+     }
+
+ string[] mass = new string[count];
     int j = 0;
     for (int i = 0; i < str.Length; i++)
     {
         if (str[i].Length <= 3)
         {
-            string[] str2 = new string[j + 1];
-            str2[j] = str[i];
-            Console.WriteLine($"This is elements less three: {str2[j]}");
+            mass[j] = str[i];
             j++;
         }
     }
+    PrintArray(mass);
 }
