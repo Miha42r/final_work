f9aa78be500d782c2fdffd9f66a29b0bf2cd5df2
Miha_GB <m-stogov@yandex.ru>
2023-02-19 17:23:49 +0700

    Добавил метод FindElementsLessThree

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -16,16 +16,18 @@ string[] Input()
 
 
 string[] str = Input();
-
-
-int j = 0;
-for (int i = 0; i < str.Length; i++)
+FindElementsLessThree(str);
+void FindElementsLessThree(string[] str)
 {
-    if (str[i].Length <= 3)
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
     {
-        string[] str2 = new string[j + 1];
-        str2[j] = str[i];
-        Console.WriteLine($"This is elements less third: {str2[j]}");
-        j++;
+        if (str[i].Length <= 3)
+        {
+            string[] str2 = new string[j + 1];
+            str2[j] = str[i];
+            Console.WriteLine($"This is elements less three: {str2[j]}");
+            j++;
+        }
     }
 }
