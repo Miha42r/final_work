a54a4daee4025b195f649130de873afc5a4d3013
Miha_GB <m-stogov@yandex.ru>
2023-02-19 17:20:07 +0700

    Добавил ввод с клавиатуры

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -1,19 +1,31 @@
 ﻿
+using System.Linq.Expressions;
 
-string[] str = { "1234", "123", "12345"};
+string[] Input()
+{
+    Console.WriteLine("Введите количество элементов");
+    int length = int.Parse(Console.ReadLine()!);
+    string[] mass = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Input[{i}]:");
+        mass[i] = Console.ReadLine()!;
+    }
+    return mass;
+}
+
+
+string[] str = Input();
+
 
 int j = 0;
 for (int i = 0; i < str.Length; i++)
 {
-
-    Console.WriteLine(j);
     if (str[i].Length <= 3)
     {
         string[] str2 = new string[j + 1];
         str2[j] = str[i];
-        Console.WriteLine(str2[j]);
+        Console.WriteLine($"This is elements less third: {str2[j]}");
         j++;
-
     }
-
 }
